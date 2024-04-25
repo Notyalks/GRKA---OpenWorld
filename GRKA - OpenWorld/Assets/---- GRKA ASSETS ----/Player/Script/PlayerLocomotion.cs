fc89431aef8604a7fe4aa39eea07a5aedfd5b471
SaLeVs <141702002+SaLeVs@users.noreply.github.com>
2024-04-25 10:06:29 -0300

    PlayerMovement

---
+++ GRKA - OpenWorld/Assets/---- GRKA ASSETS ----/Player/Script/PlayerLocomotion.cs
@@ -0,0 +1,60 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerLocomotion : MonoBehaviour
+{
+    InputManager inputManager;
+
+    Vector3 moveDirection;
+    Transform cam;
+    Rigidbody rb;
+
+    public float movementSpeed = 7;
+    public float rotationSpeed = 15;
+
+    public void Awake()
+    {
+        inputManager = GetComponent<InputManager>();
+        rb = GetComponent<Rigidbody>();
+        cam = Camera.main.transform;
+    }
+
+    public void HandleAllMovement()
+    {
+        HandleMovement();
+        HandleRotation();
+    }
+
+    private void HandleMovement()
+    {
+        moveDirection = cam.forward * inputManager.verticalInput;
+        moveDirection = moveDirection + cam.right * inputManager.horizontalInput;
+        moveDirection.Normalize();
+        moveDirection.y = 0;
+        moveDirection = moveDirection * movementSpeed;
+
+        Vector3 movementVelocity = moveDirection;
+        rb.velocity = movementVelocity;
+
+    }
+
+    private void HandleRotation()
+    {
+        Vector3 targetDirection = Vector3.zero;
+        targetDirection = cam.forward * inputManager.verticalInput;
+        targetDirection = targetDirection + cam.right * inputManager.horizontalInput;
+        targetDirection.Normalize();
+        targetDirection.y = 0;
+
+        if(targetDirection == Vector3.zero)
+        {
+            targetDirection = transform.forward;
+        }
+
+        Quaternion targetRotation = Quaternion.LookRotation(targetDirection);
+        Quaternion playerRotation = Quaternion.Slerp(transform.rotation, targetRotation, rotationSpeed * Time.deltaTime);
+
+        transform.rotation = playerRotation;    
+    }
+}
