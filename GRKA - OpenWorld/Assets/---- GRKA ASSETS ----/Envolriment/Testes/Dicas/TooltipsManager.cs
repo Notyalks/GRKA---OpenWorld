c51233b8cf9b3453aabbb7e324a79b458955d7bb
Notyalks <silvaklayton@outlook.com>
2024-06-09 17:48:49 -0300

    Tooltip adicionado
    Script TooltipManager

---
+++ GRKA - OpenWorld/Assets/---- GRKA ASSETS ----/Envolriment/Testes/Dicas/TooltipsManager.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TooltipManager : MonoBehaviour
+{
+    public static TooltipManager Instance { get; private set; }
+
+    public GameObject tooltipPanel;
+    public Text tooltipText; // Use TextMeshProUGUI for TextMeshPro or Text for regular Text
+    public float displayDuration = 2f; // Duration for how long the tooltip will be visible
+
+    private Animator animator;
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+
+        animator = tooltipPanel.GetComponent<Animator>();
+    }
+
+    void Start()
+    {
+        TooltipManager.Instance.ShowTooltip("Bem-vindo ao jogo!");
+    }
+
+    public void ShowTooltip(string message)
+    {
+        tooltipText.text = message;
+        animator.SetTrigger("Show");
+        StartCoroutine(HideTooltipAfterDelay(displayDuration));
+    }
+
+    private IEnumerator HideTooltipAfterDelay(float delay)
+    {
+        yield return new WaitForSeconds(delay);
+        animator.SetTrigger("Hide");
+    }
+}
+
